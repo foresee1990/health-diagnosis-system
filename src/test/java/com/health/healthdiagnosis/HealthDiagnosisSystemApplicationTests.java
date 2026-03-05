e694b9686f2ba13d710f437d0211be8ee008e988
foresee1990 <foresee1990@gmail.com>
2026-03-05 20:47:56 +0800

    project initialize and create database

---
+++ src/test/java/com/health/healthdiagnosis/HealthDiagnosisSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.healthdiagnosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDiagnosisSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
